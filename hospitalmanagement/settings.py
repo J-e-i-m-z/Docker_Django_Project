c25d576b96c87b3c58e6783a336eae5a4aa93d77
j-e-i-m-z <nyamburijames@gmail.com>
2025-08-23 23:08:24 +0300

    Modified settings.py: Add STATIC_ROOT for collectstatic

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[STATIC_DIR,]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # for collectstatic
+
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
 
